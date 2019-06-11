3ee18d697aae970e408f82281792eae576bea558
Nibraaska <nnibraas@gmail.com>
2019-06-11 02:52:29 -0400

    Slightly broken

--- app/src/main/java/com/nibraas/slife/MainActivity.kt
+++ app/src/main/java/com/nibraas/slife/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nibraas.slife
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
