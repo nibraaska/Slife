c43e5c5789f167ba1ca25fb642ebab3058523655
Nibraaska <nnibraas@gmail.com>
2019-06-21 16:44:10 -0400

    Updated firebase structure

---
+++ parse/seperateCountries.py
@@ -0,0 +1,99 @@
+def main():
+    fp = open("colleges.json", encoding="utf8")
+    fp2 = open("collegesfixed.json",'w', encoding='utf-8')
+
+    json_item = ""
+    country = ""
+    first = True
+    firstLoop = True
+    item_found = False
+    countryList = []
+    seperatedDict = {}
+
+    for line in fp:
+        if "{" in line:
+            item_found = True
+        if item_found:
+            json_item += line
+            if "\"country\"" in line:
+                country = line[15:-3]
+                if country not in countryList:
+                    countryList.append(country)
+        if "}" in line:
+
+            seperatedDict.setdefault(country,[]).append((json_item))
+
+            item_found = False
+            json_item = ""
+            country = ""
+
+
+    fp2.write("{\n")
+    fp2.write("\t\"colleges\" : {\n")
+    for key in sorted(seperatedDict):
+        if first:
+            fp2.write("\t\t\"" + key + "\" :\n \t\t\t{\n")
+
+            firstLoop = True
+            for item in seperatedDict[key]:
+                if firstLoop:
+                    name = item.split("\n")[1][12:-2].replace('$', ' ').replace('#', ' ').replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('.', ' ')
+                    if name == "":
+                        name = "Empty"
+                    for line in item.split("\n"):
+                        if "{" in line:
+                            fp2.write("\t\t\t\t\"" + name.strip() + "\" : {\n")
+                        elif "}" in line:
+                            fp2.write("\t\t\t" + "}")
+                        else:
+                            fp2.write("\t\t\t\t" + line.strip() + "\n")
+                    firstLoop = False
+                else:
+                    name = item.split("\n")[1][12:-2].replace('$', ' ').replace('#', ' ').replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('.', ' ')
+                    if name == "":
+                        name = "Empty"
+                    for line in item.split("\n"):
+                        if "{" in line:
+                            fp2.write("\n,\t\t\t \"" + name.strip() + "\" : {\n")
+                        elif "}" in line:
+                            fp2.write("\t\t\t" + "}")
+                        else:
+                            fp2.write("\t\t\t\t" + line.strip() + "\n")
+            
+            fp2.write("\t\t}")
+            first = False
+        else:
+            fp2.write(",\t\t\"" + key + "\" :\n \t\t\t{\n")
+
+            firstLoop = True
+            for item in seperatedDict[key]:
+                if firstLoop:
+                    name = item.split("\n")[1][12:-2].replace('$', ' ').replace('#', ' ').replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('.', ' ')
+                    if name == "":
+                        name = "Empty"
+                    for line in item.split( "\n"):
+                        if "{" in line:
+                            fp2.write("\t\t\t\t\"" + name.strip() + "\" : {\n")
+                        elif "}" in line:
+                            fp2.write("\t\t\t" + "}")
+                        else:
+                            fp2.write("\t\t\t\t" + line.strip() + "\n")
+                    firstLoop = False
+                else:
+                    name = item.split("\n")[1][12:-2].replace('$', ' ').replace('#', ' ').replace('[', ' ').replace(']', ' ').replace('/', ' ').replace('.', ' ')
+                    if name == "":
+                        name = "Empty"
+                    for line in item.split("\n"):
+                        if "{" in line:
+                            fp2.write("\n,\t\t\t \"" + name.strip() + "\" : {\n")
+                        elif "}" in line:
+                            fp2.write("\t\t\t" + "}")
+                        else:
+                            fp2.write("\t\t\t\t" + line.strip() + "\n")
+
+            fp2.write("\t\t}")
+    fp2.write("\n\t}\n")
+    fp2.write("}")
+
+
+main()
