7720d6d0da60e52c57ebd583a34843dba14a433f
Nibraaska <nnibraas@gmail.com>
2019-06-30 15:57:26 -0400

    Finished register

--- app/src/main/java/com/slife/slife/onboarding/RegisterActivity.kt
+++ app/src/main/java/com/slife/slife/onboarding/RegisterActivity.kt
@@ -1,50 +1,125 @@
 package com.slife.slife.onboarding
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.view.ViewStub
 import android.view.Window
 import android.view.WindowManager
-import android.widget.Button
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.slife.slife.R
+
 
 class RegisterActivity: AppCompatActivity() {
 
-    private lateinit var Registerbutton: Button
-
     private lateinit var viewStub: ViewStub
     private lateinit var inflatedView: View
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var registerBtn: Button
+    private lateinit var name: EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var pb: ProgressBar
+    private lateinit var alreadyCreated: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setUp()
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
         viewStub = findViewById(R.id.layout_stub)
         viewStub.layoutResource = R.layout.layout_register
         inflatedView = viewStub.inflate()
 
-        Registerbutton = inflatedView.findViewById(R.id.registerBtn)
+        inflatedView.let {
+            registerBtn = it.findViewById(R.id.registerBtn)
+            name = it.findViewById(R.id.nameRegister)
+            email = it.findViewById(R.id.emailRegister)
+            password = it.findViewById(R.id.passwordRegister)
+            confirmPassword = it.findViewById(R.id.passwordRegisterConfirm)
+            pb = it.findViewById(R.id.progressRegister)
+            alreadyCreated = it.findViewById(R.id.alreadyCreated)
+        }
 
-        Registerbutton.setOnClickListener {
-            savePrefsData()
+        registerBtn.setOnClickListener {
+            checkFieldsAndCreate()
+        }
+
+        alreadyCreated.setOnClickListener {
+            val loginActivity = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(loginActivity)
+            finish()
         }
     }
 
     private fun setUp(){
-        // Make it full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        // Hides action bar
         supportActionBar!!.hide()
     }
 
-    // Saves so on boarding doesn't happen again
+    private fun checkFieldsAndCreate(){
+        val myEmail = email.text.toString()
+        val myPassword = password.text.toString()
+        val mycPassword = confirmPassword.text.toString()
+        val myName = name.text.toString()
+
+        if(myName.isEmpty()){
+            name.error = "Please enter your name"
+            name.requestFocus()
+        }
+        else if(myEmail.isEmpty()){
+            email.error = "Please enter your email"
+            email.requestFocus()
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(myEmail).matches()){
+            email.error = "Please enter a valid email"
+            email.requestFocus()
+        }
+        else if(myPassword.isEmpty()){
+            password.error = "Please enter your password"
+            password.requestFocus()
+        }
+        else if(mycPassword.isEmpty()){
+            confirmPassword.error = "Please enter your password again"
+            confirmPassword.requestFocus()
+        }
+        else if(myPassword != mycPassword){
+            confirmPassword.error = "Passwords need to match"
+            confirmPassword.requestFocus()
+        } else {
+            pb.visibility = View.VISIBLE
+            createUser()
+        }
+    }
+
+    private fun createUser(){
+        pb.visibility = View.VISIBLE
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener(this
+            ) { p0 ->
+                pb.visibility = View.GONE
+                if (p0.isSuccessful) {
+                    savePrefsData()
+                    Toast.makeText(this@RegisterActivity, "createUserWithEmail:", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@RegisterActivity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
     private fun savePrefsData() {
         val pref = applicationContext.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
         pref.edit().putBoolean("openedBefore", true).apply()
