6413649879cb2abcaf85c748ac98dee14605aab6
Nibraaska <nnibraas@gmail.com>
2019-07-06 18:05:18 -0400

    Added autocomplete

--- app/src/main/java/com/slife/slife/BottomNavigation/UniversitiesFragment.kt
+++ app/src/main/java/com/slife/slife/BottomNavigation/UniversitiesFragment.kt
@@ -3,24 +3,27 @@ package com.slife.slife.BottomNavigation
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
-import android.widget.Spinner
-import android.widget.TextView
-import com.google.firebase.database.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.slife.slife.R
-import kotlinx.android.synthetic.main.fragment_universities.*
+
 
 class UniversitiesFragment : Fragment() {
 
-    private lateinit var countryListRef: DatabaseReference
-    private lateinit var collegesRef: DatabaseReference
+    private lateinit var countryAutoComplete: AutoCompleteTextView
+    private lateinit var collegesAutoComplete: AutoCompleteTextView
 
-    private lateinit var countryMenuText: TextView
-    private lateinit var countryMenu: PopupMenu
+    private lateinit var pb: ProgressBar
 
     private lateinit var v: View
 
@@ -31,45 +34,57 @@ class UniversitiesFragment : Fragment() {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_universities, container, false)
 
-        countryMenuText = v.findViewById(R.id.universitiesList)
-        countryMenu = PopupMenu(this.context, countryMenuText)
-        countryMenuText.setOnClickListener {
-            countryMenu.show()
-        }
+        countryAutoComplete = v.findViewById(R.id.countryList)
+        collegesAutoComplete = v.findViewById(R.id.collegesList)
+        pb = v.findViewById(R.id.progressBar)
+
+        getCountryList()
 
         return v
     }
 
     @Suppress("UNCHECKED_CAST")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        countryListRef = FirebaseDatabase.getInstance().reference
+    fun getCountryList(){
+        pb.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
             .child("CountryList")
             .child("CountryList")
+            .addValueEventListener(object: ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {}
+                override fun onDataChange(p0: DataSnapshot) {
+                    val list: ArrayList<String> = arrayListOf(p0.value)[0] as ArrayList<String>
+                    val adapter = ArrayAdapter(context!!, android.R.layout.simple_dropdown_item_1line, list)
+                    countryAutoComplete.setAdapter(adapter)
+                    pb.visibility = View.GONE
+                    countryAutoComplete.onItemClickListener =
+                        AdapterView.OnItemClickListener { parent, _, position, _ ->
+                            Log.d("here", parent?.getItemAtPosition(position).toString())
+                            collegesAutoComplete.visibility = View.VISIBLE
+                            getCollegesList()
+                        }
+                }
+            })
+    }
 
-        collegesRef = FirebaseDatabase.getInstance().reference
+    @Suppress("UNCHECKED_CAST")
+    private fun getCollegesList() {
+        pb.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
             .child("Colleges")
-
-        countryListRef.addValueEventListener(object : ValueEventListener{
-            override fun onCancelled(p0: DatabaseError) {}
-            override fun onDataChange(p0: DataSnapshot) {
-                val list: ArrayList<String> = arrayListOf(p0.value)[0] as ArrayList<String>
-                for (country in list){
-                    countryMenu.menu.add(country)
+            .child(countryAutoComplete.text.toString())
+            .addValueEventListener(object : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {}
+                override fun onDataChange(p0: DataSnapshot) {
+                    val map: Map<*, *> = p0.value as Map<*, *>
+                    val list: Array<Any?> = map.keys.toTypedArray()
+                    val adapter = ArrayAdapter(context!!, android.R.layout.simple_dropdown_item_1line, list)
+                    pb.visibility = View.GONE
+                    collegesAutoComplete.setAdapter(adapter)
+                    collegesAutoComplete.onItemClickListener =
+                        AdapterView.OnItemClickListener { parent, _, position, _ ->
+                            Log.d("here", parent?.getItemAtPosition(position).toString())
+                        }
                 }
-                countryMenu.setOnMenuItemClickListener {
-                    collegesRef.child(it.toString())
-                        .addValueEventListener(object : ValueEventListener{
-                            override fun onCancelled(p0: DatabaseError) {}
-
-                            override fun onDataChange(p0: DataSnapshot) {
-                                Log.d("here", p0.toString())
-                            }
-                        })
-                    true
-                }
-            }
-        })
+            })
     }
 }
