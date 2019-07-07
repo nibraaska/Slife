a6df544cb915c8b987a6d1b2b1b1cc29a787d2f8
Nibraaska <nnibraas@gmail.com>
2019-07-07 16:09:25 -0400

    Added more connections

---
+++ app/src/main/java/com/slife/slife/BottomNavigation/Universities/Models/College.kt
@@ -0,0 +1,86 @@
+package com.slife.slife.BottomNavigation.Universities.Models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class College (
+    @SerializedName("acronym")
+    @Expose
+    val acronym: String,
+
+    @SerializedName("addressLocality")
+    @Expose
+    val addressLocality: String,
+
+    @SerializedName("addressRegion")
+    @Expose
+    val addressRegion: String,
+
+    @SerializedName("colors")
+    @Expose
+    val colors: String,
+
+    @SerializedName("country")
+    @Expose
+    val country: String,
+
+    @SerializedName("description")
+    @Expose
+    val description: String,
+
+    @SerializedName("facebook")
+    @Expose
+    val facebook: String,
+
+    @SerializedName("fax")
+    @Expose
+    val fax: String,
+
+    @SerializedName("foundingDate")
+    @Expose
+    val foundingDate: String,
+
+    @SerializedName("instagram")
+    @Expose
+    val instagram: String,
+
+    @SerializedName("link")
+    @Expose
+    val link: String,
+
+    @SerializedName("linkedin")
+    @Expose
+    val linkedin: String,
+
+    @SerializedName("motto")
+    @Expose
+    val motto: String,
+
+    @SerializedName("name")
+    @Expose
+    val name: String,
+
+    @SerializedName("otherlocations")
+    @Expose
+    val otherlocations: String,
+
+    @SerializedName("postalCode")
+    @Expose
+    val postalCode: String,
+
+    @SerializedName("streetAddress")
+    @Expose
+    val streetAddress: String,
+
+    @SerializedName("telephone")
+    @Expose
+    val telephone: String,
+
+    @SerializedName("twitter")
+    @Expose
+    val twitter: String,
+
+    @SerializedName("youtube")
+    @Expose
+    val youtube: String
+)
--- app/src/main/java/com/slife/slife/BottomNavigation/Universities/View/UniversitiesFragment.kt
+++ app/src/main/java/com/slife/slife/BottomNavigation/Universities/View/UniversitiesFragment.kt
@@ -1,22 +1,16 @@
 package com.slife.slife.BottomNavigation.Universities.View
 
 
-import android.app.Application
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.slife.slife.BottomNavigation.Universities.Models.College
+import com.slife.slife.BottomNavigation.Universities.Models.CollegesInCountry
 import com.slife.slife.BottomNavigation.Universities.Models.CountryList
 import com.slife.slife.Networking.MobileAPI
 import com.slife.slife.Networking.MobileCallBack
@@ -32,8 +26,7 @@ class UniversitiesFragment : Fragment() {
 
     private lateinit var v: View
 
-
-    private var mapi= MobileAPI("https://studyout-5e59e.firebaseio.com/")
+    private lateinit var mAPI: MobileAPI
 
 
     override fun onCreateView(
@@ -43,15 +36,24 @@ class UniversitiesFragment : Fragment() {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_universities, container, false)
 
+        mAPI = MobileAPI(resources.getString(R.string.BASE_URL))
+
         countryAutoComplete = v.findViewById(R.id.countryList)
         collegesAutoComplete = v.findViewById(R.id.collegesList)
         pb = v.findViewById(R.id.progressBar)
 
-//        getCountryList()
-
-        mapi.getCountryList(object : MobileCallBack<CountryList> {
+        mAPI.getCountryList(object : MobileCallBack<CountryList> {
             override fun success(data: CountryList) {
                 Log.d("vm", data.countryList[0])
+            }
+            override fun failure(message: String) {
+                Log.d("vm", message)
+            }
+        })
+
+        mAPI.getCollegesInCountry("Morocco", object : MobileCallBack<CollegesInCountry> {
+            override fun success(data: CollegesInCountry) {
+                Log.d("vm", data.collegeList.toString())
             }
 
             override fun failure(message: String) {
@@ -59,67 +61,16 @@ class UniversitiesFragment : Fragment() {
             }
         })
 
+        mAPI.getCollege("Morocco", "Universite Cadi Ayyad", object : MobileCallBack<College> {
+            override fun success(data: College) {
+                Log.d("vm", data.toString())
+            }
+
+            override fun failure(message: String) {
+                Log.d("vm", message)
+            }
+
+        })
         return v
     }
-
-//    @Suppress("UNCHECKED_CAST")
-//    fun getCountryList(){
-//        pb.visibility = View.VISIBLE
-//        FirebaseDatabase.getInstance().reference
-//            .child("CountryList")
-//            .child("CountryList")
-//            .addValueEventListener(object: ValueEventListener{
-//                override fun onCancelled(p0: DatabaseError) {}
-//                override fun onDataChange(p0: DataSnapshot) {
-//                    val list: ArrayList<String> = arrayListOf(p0.value)[0] as ArrayList<String>
-//                    val adapter = ArrayAdapter(context!!, android.R.layout.simple_dropdown_item_1line, list)
-//                    countryAutoComplete.setAdapter(adapter)
-//                    pb.visibility = View.GONE
-//                    countryAutoComplete.onItemClickListener =
-//                        AdapterView.OnItemClickListener { parent, _, position, _ ->
-//                            collegesAutoComplete.visibility = View.VISIBLE
-//                            getCollegesList(parent.getItemAtPosition(position).toString())
-//                        }
-//                }
-//            })
-//    }
-//
-//    @Suppress("UNCHECKED_CAST")
-//    private fun getCollegesList(country: String) {
-//        pb.visibility = View.VISIBLE
-//        FirebaseDatabase.getInstance().reference
-//            .child("Colleges")
-//            .child(country)
-//            .addValueEventListener(object : ValueEventListener{
-//                override fun onCancelled(p0: DatabaseError) {}
-//                override fun onDataChange(p0: DataSnapshot) {
-//                    val map: Map<*, *> = p0.value as Map<*, *>
-//                    val list: Array<Any?> = map.keys.toTypedArray()
-//                    val adapter = ArrayAdapter(context!!, android.R.layout.simple_dropdown_item_1line, list)
-//                    pb.visibility = View.GONE
-//                    collegesAutoComplete.setAdapter(adapter)
-//                    collegesAutoComplete.onItemClickListener =
-//                        AdapterView.OnItemClickListener { parent, _, position, _ ->
-//                            getCollege(country, parent?.getItemAtPosition(position).toString())
-//                        }
-//                }
-//            })
-//    }
-//
-//    private fun getCollege(country: String, college: String) {
-//        Log.d("here", "$country, $college")
-//        pb.visibility = View.VISIBLE
-//        FirebaseDatabase.getInstance().reference
-//            .child("Colleges")
-//            .child(country)
-//            .child(college)
-//            .addValueEventListener(object: ValueEventListener{
-//                override fun onCancelled(p0: DatabaseError) {}
-//                override fun onDataChange(p0: DataSnapshot) {
-//                    pb.visibility = View.GONE
-//                    val map: Map<*, *> = p0.value as Map<*, *>
-//                    Log.d("here", map["acronym"].toString())
-//                }
-//            })
-//    }
 }
